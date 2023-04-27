ebc4dc66b23b85ba5eccc9c9f3681d79051bab59
dexpet <dexteropeterson@gmail.com>
2023-04-27 18:42:09 -0500

    Fixed functionality so now it should never have 0.00 as a solution

--- scripts/customer.js
+++ scripts/customer.js
@@ -3,12 +3,10 @@
 // create a new coinPile object so we can fill the data
 newCoinPile = new coinPile("", 0)
 
-
-
 function coinRequest() {
 
   // generate a random number
-newCoinPile.count = Math.ceil(Math.random() * 4);
+  newCoinPile.count = Math.ceil(Math.random() * 4);
 
   // assign the value based on what our number generated was
 
@@ -46,22 +44,29 @@ function customerRequest() {
   var csReqest = document.getElementById('customerRequest');
   coinTotal = 0;
 
+  //All of the console logs are for debugging and is DEV code only
   coinPile1 = coinRequest();
-  console.log(coinPile1.type);
-  console.log(coinPile1.count);
-  console.log(coinPile1.getValue());
+  console.log("First coinPile Type: " + coinPile1.type);
+  console.log("Number of coins in first coinPile: " + coinPile1.count);
+  console.log("Value of first coinPile: " + coinPile1.getValue());
+  console.log("CoinTotal prior to addition of first coinPile: " + coinTotal)
+  coinTotal += coinPile1.getValue()
+  
   coinPile2 = coinRequest();
-  console.log(coinPile2.type);
-  console.log(coinPile2.count);
-  console.log(coinPile2.getValue());
-  coinPile3 = coinRequest();
-  console.log(coinPile3.type);
-  console.log(coinPile3.count);
-  console.log(coinPile3.getValue());
+  console.log("Second coinPile Type: " + coinPile2.type);
+  console.log("Number of coins in second coinPile: " + coinPile2.count);
+  console.log("Value of second coinPile: " + coinPile2.getValue());
+  console.log("CoinTotal prior to addition of second coinPile: " + coinTotal)
+  coinTotal += coinPile2.getValue()
 
-  coinTotal += coinPile1.getValue();
-  coinTotal += coinPile2.getValue();
-  coinTotal += coinPile3.getValue();
+  coinPile3 = coinRequest();
+  console.log("Second coinPile Type: " + coinPile3.type);
+  console.log("Number of coins in second coinPile: " + coinPile3.count);
+  console.log("Value of second coinPile: " + coinPile3.getValue());
+  console.log("CoinTotal prior to addition of second coinPile: " + coinTotal)
+  coinTotal += coinPile3.getValue()
+
+  console.log(coinTotal);
 
 
   csReqest.innerText = "Hi! I need: \n" + coinTotal + "\n in change please!";
