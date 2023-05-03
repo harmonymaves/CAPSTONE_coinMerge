5dae5da3f276f98b85326d579c1c0fce8277eef8
Davin Haley <dhaley4@student.cvtc.edu>
2023-05-02 21:46:24 -0500

    [Finishes #185083697] Fix tds keeping coin data

--- scripts/custCoinCheck.js
+++ scripts/custCoinCheck.js
@@ -41,6 +41,8 @@ function coinToCustomer(td) {
 
             td.dataset.type = heldCoin.type; // coin's data to the td
             td.dataset.count = heldCoin.count; // coin's numeric amount to the td
+            heldCoinTd.dataset.type = ""; // Bug fix: clear the data from the original td.
+            heldCoinTd.dataset.count = 0;
 
             // clear the coin from the cursor because it's no longer being held
             coinBoard.style = `cursor: auto;`;
